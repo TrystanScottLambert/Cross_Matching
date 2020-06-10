2580fbc5167d21c32621c9d4176886e82e56e207
Trystan Scott Lambert <65018651+TrystanScottLambert@users.noreply.github.com>
2020-06-10 13:58:37 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setuptools
+
+setup(
+	name='Cross_Matching',
+	version='0.1',
+	author = 'Trystan Scott Lambert',
+	author_email = 'TrystanScottLambert@gmail.com',
+	description = 'Cross Matching Astronomical Catalogues',
+	url = 'https://github.com/TrystanScottLambert/Cross_Matching',
+	packages =  ['Cross_Matching'],
+)
