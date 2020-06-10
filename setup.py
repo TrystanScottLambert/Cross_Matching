31ee5872cb82065ad3882155ac166f6168a4f890
Trystan Scott Lambert <65018651+TrystanScottLambert@users.noreply.github.com>
2020-06-10 13:59:53 +0200

    updating version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
 	name='Cross_Matching',
-	version='0.1',
+	version='0.1.1',
 	author = 'Trystan Scott Lambert',
 	author_email = 'TrystanScottLambert@gmail.com',
 	description = 'Cross Matching Astronomical Catalogues',
